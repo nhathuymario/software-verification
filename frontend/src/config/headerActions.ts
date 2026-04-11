96cbfe651bc81d02094afed7582baa7ac0e3eb87
nhathuymario <huyne1501@gmail.com>
2026-04-11 14:16:12 +0700

    test cicd

--- frontend/src/config/headerActions.ts
+++ frontend/src/config/headerActions.ts
@@ -1,59 +1,68 @@
 export type HeaderAction = {
-    key: string
-    label: string
-    to: string
-    roles?: string[] // không có => ai cũng thấy
-    icon?: string
-}
+  key: string;
+  label: string;
+  to: string;
+  roles?: string[]; // không có => ai cũng thấy
+  icon?: string;
+};
 
 export const HEADER_ACTIONS: HeaderAction[] = [
-    // ✅ Lecturer
-    {
-        key: 'lecturer-create-syllabus',
-        label: 'Tạo giáo trình',
-        to: '/lecturer/syllabus/new',
-        roles: ['LECTURER'],
-        icon: '📚',
-    },
+  // ✅ Lecturer
+  {
+    key: "lecturer-create-syllabus",
+    label: "Tạo giáo trình.",
+    to: "/lecturer/syllabus/new",
+    roles: ["LECTURER"],
+    icon: "📚",
+  },
 
-    // ✅ AA
-    { key:"aa-create-course", label:"Tạo môn học", to:"/aa/courses/new", roles:["AA"], icon:"➕" },
-    { key:"aa-set-relations", label:"Set tiên quyết", to:"/aa/courses/relations", roles:["AA"], icon:"🧩" },
+  // ✅ AA
+  {
+    key: "aa-create-course",
+    label: "Tạo môn học",
+    to: "/aa/courses/new",
+    roles: ["AA"],
+    icon: "➕",
+  },
+  {
+    key: "aa-set-relations",
+    label: "Set tiên quyết",
+    to: "/aa/courses/relations",
+    roles: ["AA"],
+    icon: "🧩",
+  },
 
-    {
-        key: "aa-plos",
-        label: "Quản lý PLO",
-        to: "/aa/plos",
-        roles: ["AA"],
-        icon: "🎯",
-    },
-    {
-        key: "aa-manage-course",
-        label: "Quản lý Course",
-        to: "/aa/courses-manager",
-        roles: ["AA"],
-        icon: "📒",
-    },
-    // ✅ STUDENT
-    {
-        key: 'student-register-course',
-        label: 'Đăng ký môn học',
-        to: '/student/courses/register',
-        roles: ['STUDENT'],
-        icon: '📝',
-    },
-    // REVIEW SYLLABUS
-    {
-        key: "my_reviews",
-        label: "My Reviews",
-        to: "/reviews",
-        roles: ["LECTURER", "AA", "HOD"],
-        icon: "📌",
-    },
+  {
+    key: "aa-plos",
+    label: "Quản lý PLO",
+    to: "/aa/plos",
+    roles: ["AA"],
+    icon: "🎯",
+  },
+  {
+    key: "aa-manage-course",
+    label: "Quản lý Course",
+    to: "/aa/courses-manager",
+    roles: ["AA"],
+    icon: "📒",
+  },
+  // ✅ STUDENT
+  {
+    key: "student-register-course",
+    label: "Đăng ký môn học",
+    to: "/student/courses/register",
+    roles: ["STUDENT"],
+    icon: "📝",
+  },
+  // REVIEW SYLLABUS
+  {
+    key: "my_reviews",
+    label: "My Reviews",
+    to: "/reviews",
+    roles: ["LECTURER", "AA", "HOD"],
+    icon: "📌",
+  },
 
-
-
-
-    // Ví dụ sau này thêm:
-    // { key:'admin-users', label:'Quản lý user', to:'/admin/users', roles:['ADMIN'], icon:'🛠️' },
+  // Ví dụ sau này thêm:
+  // { key:'admin-users', label:'Quản lý user', to:'/admin/users', roles:['ADMIN'], icon:'🛠️' },
 ];
